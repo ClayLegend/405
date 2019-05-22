4a619f8e645e33f9329b4908a96750f52237b4dc
Clay Damon <clay@clays-air.attlocal.net>
2019-05-21 21:27:44 -0700

    Fourth Edit

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
 var app = require('http').createServer(response);
 var fs = require('fs');
 var io = require('socket.io')(app);
-
 app.listen(3000);
 console.log("App running...");
 
 function response(req, res) {
     var file = "";
-    if (req.url == "/") {
-        file = __dirname + '/index.html';
+    if(req.url == "/"){
+	   file = __dirname + '/index.html';
     } else {
-        file = __dirname + req.url;
+	   file = __dirname + req.url;
     }
+   
+    fs.readFile(file,
+	    function (err, data) {
+			if (err) {
+				res.writeHead(404);
+				return res.end('Page or file not found');
+			}
 
-    fs.readFile(file, function(err, data) {
-        if (err) {
-            res.writeHead(404);
-            return res.end('Page or file not found');
-        }
-
-    res.writeHead(200);
-        res.end(data);
-    });
+			res.writeHead(200);
+			res.end(data);
+	    }
+    );
 }
 
+io.sockets.on('connection', function(socket) {
+    socket.on('username', function(username) {
+        socket.username = username;
+        io.emit('is_online', '🔵 <i>' + socket.username + ' joined the chat..</i>');
+    });
+
+    socket.on('disconnect', function(username) {
+        io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
+    })
+
+    socket.on('chat_message', function(message) {
+        io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
+    });
+
+});
+
 io.on("connection", function(socket){
-    socket.on("send message", function(sent_msg, callback){
-		sent_msg = "[ " + getCurrentDate() + " ]: " + sent_msg;
+	socket.on("send message", function(sent_msg, callback){
+		sent_msg = socket.username + "[ " + getCurrentDate() + " ]: " + sent_msg;
 
 		io.sockets.emit("update messages", sent_msg);
 		callback();
-    });
+ 
+	});
+
 });
 
 function getCurrentDate(){
@@ -44,3 +63,4 @@ function getCurrentDate(){
 
 	return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
 }
+
